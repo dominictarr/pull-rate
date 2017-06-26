0563d59e369046e1b4e5a2b22a4495de134070c6
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-26 17:56:06 +1200

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# pull-rate
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,30 @@
+var pull = require('pull-stream/pull')
+
+//up, down * loopback, network, internet
+
+function Sensor (onRate) {
+  if(!onRate)
+    return function (read) { return read }
+  return pull.through(function (data) {
+    onRate(data.length)
+  })
+}
+
+module.exports = function Rate (stream, onRate) {
+  if(stream.length == 1) //sink
+    return pull(Sensor(onRate, false), stream)
+  else if(stream.length == 2)
+    return pull(stream, Sensor(onRate, true))
+  else
+    return {
+      source: Rate(stream.source, onRate), sink: Rate(stream.sink, onRate),
+      address: stream.address
+    }
+}
+
+
+
+
+
+
+
