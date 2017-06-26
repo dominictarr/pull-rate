a6935ed28037752e30fd1a9a35b0fdcafdda7aca
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-26 18:49:04 +1200

    track up/down separately

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var pull = require('pull-stream/pull')
+var Through = require('pull-stream/throughs/through')
 
 //up, down * loopback, network, internet
 
 function Sensor (onRate, up) {
   if(!onRate)
     return function (read) { return read }
-  return pull.through(function (data) {
+  return Through(function (data) {
     onRate(data.length, up)
   })
 }
 
 module.exports = function Rate (stream, onRate) {
   if(stream.length == 1) //sink
-    return pull(Sensor(onRate, false), stream)
-  else if(stream.length == 2)
-    return pull(stream, Sensor(onRate, true))
+    return pull(Sensor(onRate, true), stream)
+  else if(stream.length == 2) //source
+    return pull(stream, Sensor(onRate, false))
   else
     return {
       source: Rate(stream.source, onRate), sink: Rate(stream.sink, onRate),
